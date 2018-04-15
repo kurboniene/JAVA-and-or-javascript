a198820ef5a5f131e14a7b8f0d5f99b7ac874c35
Kristina Urboniene <38406668+kurboniene@users.noreply.github.com>
2018-04-15 23:01:40 +0300

    Create Continents.java
    LEARN JAVA
    Continents and Cities
    Let's practice the Java switch statement that you learned about.
    
    In this project, you will write a program that will print out a continent and the largest city in that continent, based on the value of an integer.
    
    The instructions provided are general guidelines. Upon completion of the project, feel free to add functionality of your own.

---
+++ Continents.java
@@ -0,0 +1,34 @@
+//author: Kristina
+//date: 15/04/2018
+//description (with switch)
+
+
+public class Continents {
+	public static void main(String[] args) {
+
+    int continent = 3;
+		switch(continent){
+      case 1:
+      System.out.println("North America: Mexico City, Mexico");
+        break;
+      case 2:
+      System.out.println("South America: Sao Paulo, Brazil");
+        break;
+      case 3:
+      System.out.println("Africa: Lagos, Nigeria");
+        break;
+      case 5:
+      System.out.println("Asia: Shanghai, China");
+        break;
+      case 6:
+      System.out.println("Australia: Sydney, Australia");
+        break;
+      case 7:
+      System.out.println("Antarctica: McMurdo Station, US");
+        break;
+      default:
+      System.out.println("Undefined continent!");
+    }
+
+	}
+}
